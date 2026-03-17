1509dd8c677102776f392c9a2ea607facee93478
이다은 <daeun020122@naver.com>
2026-03-17 17:47:14 +0900

    Modify css with GPT

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
 
     return(
     <div className="Header">
-        <h3>Today is</h3>
-        <h1>{today.getFullYear() + '년' + (today.getMonth()+1) + '월' + today.getDate() + '일' + day[today.getDay()] + '요일'}</h1>
+        <h3>Today is...</h3>
+        <h1>{today.getFullYear() + '년 ' + (today.getMonth()+1) + '월 ' + today.getDate() + '일 ' + day[today.getDay()] + '요일'}</h1>
     </div>
     );
 };
